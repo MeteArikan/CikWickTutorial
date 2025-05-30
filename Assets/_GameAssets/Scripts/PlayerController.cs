b8735be53f4808928e677bd647c0ff6dd62d8b3a
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-05-30 12:42:52 +0300

    V0.2.2 Added Chick Movement

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    [SerializeField] private Transform _orientationTransform;
+
+
+    [Header("Movement Settings")]
+    [SerializeField] private float _moveSpeed = 20f;
+
+
+    private Rigidbody _playerRigidbody;
+
+    private float _horizontalInput, _verticalInput;
+
+    private Vector3 _moveDirection;
+
+
+    private void Awake()
     {
-        
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.freezeRotation = true;
+        _playerRigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+        _playerRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+    private void Update()
+    {
+        SetInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        SetPlayerMovement();
     }
+
+
+    private void SetInput()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void SetPlayerMovement()
+    {
+        _moveDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
+
+        _playerRigidbody.AddForce(_moveDirection * _moveSpeed, ForceMode.Force);
+    }
+
+
+
+
+
+
 }
