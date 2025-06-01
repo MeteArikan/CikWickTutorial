f47dd50092fc07bda638191d8111695fe29cbd13
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-06-01 18:41:16 +0300

    v0.3 Chick Controller Added
    Slide is also added alongside to the previous movement and jumping

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +10,7 @@ public class PlayerController : MonoBehaviour
 
     [Header("Movement Settings")]
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private KeyCode _moveKey;
 
     [Header("Jumping Settings")]
     [SerializeField] private KeyCode _jumpKey;
@@ -16,9 +18,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool _canJump;
     [SerializeField] private float _jumpCooldown;
 
+    [Header("Sliding Settings")]
+    [SerializeField] private KeyCode _slideKey;
+    [SerializeField] private float _slideMultiplier;
+    [SerializeField] private float _slideDrag;
+    
+
     [Header("Ground Check Settings")]
     [SerializeField] private float _playerHeight;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundDrag;
+
 
     private Rigidbody _playerRigidbody;
 
@@ -26,28 +36,42 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 _moveDirection;
 
+    private bool _isSliding;
+
 
     private void Awake()
     {
-        _moveSpeed = 20f;
+        // Movement
+        _moveSpeed = 30f;
+        _moveKey = KeyCode.E;
         _playerRigidbody = GetComponent<Rigidbody>();
         _playerRigidbody.freezeRotation = true;
         _playerRigidbody.interpolation = RigidbodyInterpolation.Interpolate;
         _playerRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        _groundDrag = 5f;
 
-
+        // Jumping
         _jumpKey = KeyCode.Space;
         _canJump = true;
-        _jumpForce = 5f;
+        _jumpForce = 8f;
         _jumpCooldown = 0.5f;
 
+        // Ground Check
         _playerHeight = 1f;
         _groundLayer = LayerMask.GetMask("Ground");
+
+        // Sliding
+        _slideKey = KeyCode.Q;
+        _slideMultiplier = 2f;
+        _slideDrag = 2f;
 
     }
     private void Update()
     {
         SetInputs();
+        SetPlayerDrag();
+        LimitPlayerVelocity();
+
     }
 
     private void FixedUpdate()
@@ -61,7 +85,18 @@ public class PlayerController : MonoBehaviour
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(_jumpKey) && _canJump && IsGrounded())
+        if (Input.GetKeyDown(_slideKey))
+        {
+            _isSliding = true;
+            Debug.Log("Sliding");
+        }
+        else if (Input.GetKeyDown(_moveKey))
+        {
+            _isSliding = false;
+            Debug.Log("Moving");
+        }
+
+        else if (Input.GetKey(_jumpKey) && _canJump && IsGrounded())
         {
             _canJump = false;
             SetPlayerJumping();
@@ -73,13 +108,41 @@ public class PlayerController : MonoBehaviour
     {
         _moveDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
 
-        _playerRigidbody.AddForce(_moveDirection.normalized * _moveSpeed, ForceMode.Force);
+        float forceMultiplier = 1f;
+        if (_isSliding)
+        {
+            forceMultiplier = _slideMultiplier;
+        }
+
+        _playerRigidbody.AddForce(_moveDirection.normalized * _moveSpeed * forceMultiplier, ForceMode.Force);
     }
 
     private void SetPlayerJumping()
     {
         _playerRigidbody.linearVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
         _playerRigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void SetPlayerDrag()
+    {
+        if(_isSliding)
+        {
+            _playerRigidbody.linearDamping = _slideDrag;
+        }
+        else
+        {
+            _playerRigidbody.linearDamping = _groundDrag;
+        }
+    }
+
+    private void LimitPlayerVelocity()
+    {
+        Vector3 flatVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+        if (flatVelocity.magnitude > _moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * _moveSpeed;
+            _playerRigidbody.linearVelocity = new Vector3(limitedVelocity.x, _playerRigidbody.linearVelocity.y, limitedVelocity.z);
+        }
     }
 
     private void ResetJumping()
