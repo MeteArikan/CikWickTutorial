fd2a28554b7dac978e461533287f591393cc431e
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-06-18 21:26:29 +0300

    Revert "TEST: Rotation Animation"
    This reverts commit 7ae077ce3b642211e5f8cbbd27c1289c2b1f8566.

--- Assets/_GameAssets/Scripts/UI/TimerUI.cs
+++ Assets/_GameAssets/Scripts/UI/TimerUI.cs
@@ -1,7 +1,6 @@
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class TimerUI : MonoBehaviour
 {
@@ -12,40 +11,18 @@ public class TimerUI : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private float _rotationDuration;
-    [SerializeField] private float _rotationDurationTest;
     [SerializeField] private Ease _rotationEase;
 
-    private bool _isRotating;
 
 
-
-
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            if (!_isRotating)
-            {
-                PlayClockAnimationTest();
-            }
-            
-        }
+    private void Start() {
+        PlayClockAnimation();
     }
     private void PlayClockAnimation()
     {
         _timerRotatableTransform.DORotate(new Vector3(0f, 0f, -360f), _rotationDuration, RotateMode.FastBeyond360)
             .SetEase(_rotationEase)
             .SetLoops(-1, LoopType.Restart);
-
-    }
-
-    private void PlayClockAnimationTest()
-    {
-        _isRotating = true;
-        _timerRotatableTransform.DORotate(new Vector3(0f, 0f,  - 30f), _rotationDurationTest, RotateMode.LocalAxisAdd)
-            .SetEase(_rotationEase).OnComplete(() =>
-            {
-                _isRotating = false;
-            });
 
     }
 
