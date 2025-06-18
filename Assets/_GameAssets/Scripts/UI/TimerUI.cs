7ae077ce3b642211e5f8cbbd27c1289c2b1f8566
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-06-18 21:26:14 +0300

    TEST: Rotation Animation

--- Assets/_GameAssets/Scripts/UI/TimerUI.cs
+++ Assets/_GameAssets/Scripts/UI/TimerUI.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TimerUI : MonoBehaviour
 {
@@ -11,18 +12,40 @@ public class TimerUI : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private float _rotationDuration;
+    [SerializeField] private float _rotationDurationTest;
     [SerializeField] private Ease _rotationEase;
 
+    private bool _isRotating;
 
 
-    private void Start() {
-        PlayClockAnimation();
+
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!_isRotating)
+            {
+                PlayClockAnimationTest();
+            }
+            
+        }
     }
     private void PlayClockAnimation()
     {
         _timerRotatableTransform.DORotate(new Vector3(0f, 0f, -360f), _rotationDuration, RotateMode.FastBeyond360)
             .SetEase(_rotationEase)
             .SetLoops(-1, LoopType.Restart);
+
+    }
+
+    private void PlayClockAnimationTest()
+    {
+        _isRotating = true;
+        _timerRotatableTransform.DORotate(new Vector3(0f, 0f,  - 30f), _rotationDurationTest, RotateMode.LocalAxisAdd)
+            .SetEase(_rotationEase).OnComplete(() =>
+            {
+                _isRotating = false;
+            });
 
     }
 
