ce0035dd0364cfdacafec0bafad4d5ce66a5e8c3
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-06-18 17:53:36 +0300

    TEST:Rotten Wheat Damage
    A simple  test for experimenting with Unity. A bonus attribıte is added to the rotten wheat where it damages user when it is collected. If the player's health is 3, the damages reduces 2 health points and if the player has less life points, it reduces 1.

--- Assets/_GameAssets/Scripts/UI/PlayerHealthUI.cs
+++ Assets/_GameAssets/Scripts/UI/PlayerHealthUI.cs
@@ -27,35 +27,59 @@ public class PlayerHealthUI : MonoBehaviour
     }
 
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            AnimateDamage();
-        }
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            AnimateDamageForAll();
-        }
-    }
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.O))
+    //     {
+    //         AnimateDamage();
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.P))
+    //     {
+    //         AnimateDamageForAll();
+    //     }
+    // }
+
+    // public void AnimateDamage()
+    // {
+    //     for (int i = 0; i < _playerHealthImages.Length; i++)
+    //     {
+    //         if (_playerHealthImages[i].sprite == _healthySprite)
+    //         {
+    //             AnimateDamageSprites(_playerHealthImages[i], _playerHealthTransforms[i]);
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // public void AnimateDamageForAll()
+    // {
+    //     for (int i = 0; i < _playerHealthImages.Length; i++)
+    //     {
+    //         AnimateDamageSprites(_playerHealthImages[i], _playerHealthTransforms[i]);
+    //     }
+    // }
 
     public void AnimateDamage()
     {
-        for (int i = 0; i < _playerHealthImages.Length; i++)
-        {
-            if (_playerHealthImages[i].sprite == _healthySprite)
-            {
-                AnimateDamageSprites(_playerHealthImages[i], _playerHealthTransforms[i]);
-                break;
-            }
-        }
+        AnimateDamage(1);
     }
 
     public void AnimateDamageForAll()
     {
-        for (int i = 0; i < _playerHealthImages.Length; i++)
-        {   
-            AnimateDamageSprites(_playerHealthImages[i], _playerHealthTransforms[i]);  
+        AnimateDamage(_playerHealthImages.Length);
+    }
+
+    public void AnimateDamage(int damageCount)
+    {
+        int animatedCount = 0;
+        
+        for (int i = 0; i < _playerHealthImages.Length && animatedCount < damageCount; i++)
+        {
+            if (_playerHealthImages[i].sprite == _healthySprite)
+            {
+                AnimateDamageSprites(_playerHealthImages[i], _playerHealthTransforms[i]);
+                animatedCount++;
+            }
         }
     }
 
