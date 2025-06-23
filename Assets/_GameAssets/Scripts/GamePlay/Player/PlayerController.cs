2e1aa774bc6f09cead9f5c41cc6c7621cf489a15
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-06-23 23:35:06 +0300

    v0.15.0 Settings PopUp Added

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
@@ -82,6 +82,11 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
+        if (GameManager.Instance.GetCurrentGameState() != GameState.Play
+        && GameManager.Instance.GetCurrentGameState() != GameState.Resume)
+        {
+            return;
+        }
         SetInputs();
         SetStates();
         SetPlayerDrag();
@@ -91,6 +96,11 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (GameManager.Instance.GetCurrentGameState() != GameState.Play
+        && GameManager.Instance.GetCurrentGameState() != GameState.Resume)
+        {
+            return;
+        }
         SetPlayerMovement();
     }
 
--- Assets/_GameAssets/Scripts/Managers/GameManager.cs
+++ Assets/_GameAssets/Scripts/Managers/GameManager.cs
@@ -1,20 +1,37 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public event Action<GameState> OnGameStateChanged;
 
     [Header("References")]
     [SerializeField] private EggCounterUI _eggCounterUI;
 
     [Header("Settings")]
     [SerializeField] private int _maxEggCount = 5;
-    
+
+
+    private GameState _currentGameState;
     private int _currentEggCount;
+
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        ChangeGameState(GameState.Play);
+    }
+    public void ChangeGameState(GameState gameState)
+    {
+        OnGameStateChanged?.Invoke(gameState);
+        _currentGameState = gameState;
+        Debug.Log("Game State: " + gameState);
     }
     public void OnEggCollected()
     {
@@ -24,7 +41,14 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log("Game Win");
             _eggCounterUI.SetEggCompleted();
+            ChangeGameState(GameState.GameOver);
         }
         Debug.Log("Egg collected! Current count: " + _currentEggCount);
     }
+
+    public GameState GetCurrentGameState()
+    {
+        return _currentGameState;
+    }
+
 }
