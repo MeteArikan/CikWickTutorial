f8e370fd99246f5bb835356d2968b8ed357682ef
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-06-26 02:11:17 +0300

    v0.15.1 Timer Pause Added

--- Assets/_GameAssets/Scripts/UI/TimerUI.cs
+++ Assets/_GameAssets/Scripts/UI/TimerUI.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -14,33 +15,73 @@ public class TimerUI : MonoBehaviour
     [SerializeField] private Ease _rotationEase;
 
     private float _elapsedTine;
-
+    private bool _isTimerRunning;
+    private Tween _rotationTween;
 
 
     private void Start()
     {
         PlayClockAnimation();
         StartTimer();
+        GameManager.Instance.OnGameStateChanged += GameManager_OnGameStateChanged;
     }
+
+    private void GameManager_OnGameStateChanged(GameState gameState)
+    {
+        switch (gameState)
+        {
+            case GameState.Pause:
+                PauseTimer();
+                break;
+
+            case GameState.Resume:
+                ResumeTimer();
+                break;
+        }
+    }
+
+    private void PauseTimer()
+    {
+        _isTimerRunning = false;
+        CancelInvoke(nameof(UpdateTimerUI));
+        _rotationTween.Pause();
+
+    }
+
+    private void ResumeTimer()
+    {
+        if (!_isTimerRunning)
+        {
+            _isTimerRunning = true;
+            InvokeRepeating(nameof(UpdateTimerUI), 0f, 1f);
+            _rotationTween.Play();
+            
+        }
+    }
+
+
+
     private void PlayClockAnimation()
     {
-        _timerRotatableTransform.DORotate(new Vector3(0f, 0f, -360f), _rotationDuration, RotateMode.FastBeyond360)
+       _rotationTween = _timerRotatableTransform.DORotate(new Vector3(0f, 0f, -360f), _rotationDuration, RotateMode.FastBeyond360)
             .SetEase(_rotationEase)
             .SetLoops(-1, LoopType.Restart);
 
     }
     private void StartTimer()
     {
+        _isTimerRunning = true;
         _elapsedTine = 0f;
         InvokeRepeating(nameof(UpdateTimerUI), 0f, 1f);
     }
 
     private void UpdateTimerUI()
     {
+        if (!_isTimerRunning) { return; }
+
         _elapsedTine += 1f;
         int minutes = Mathf.FloorToInt(_elapsedTine / 60f);
         int seconds = Mathf.FloorToInt(_elapsedTine % 60f);
-
         _timerText.text = $"{minutes:00}:{seconds:00}";
     }
 
